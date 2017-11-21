7286034abafc79d01d5611d7fd8f7f3f952a78f5
Lien Ly <liely@deloitte.com>
2017-11-21 12:21:19 -0700

    Add cards and their flip functionalities

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Number of times cards are flipped
+    var flipCount: Int = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardIdx = cardButtons.index(of: sender) {
+            // flipCard here
+        }
+        else {
+            print("Chosen card is not in cardButtons")
+        }
     }
-
-
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        // Flip card faceUp or faceDown
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        }
+        else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 }
 
